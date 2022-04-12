d60e7b429b70a77c18fdf2cb869d7601ec4d4f26
KiMberly006 <kimberly.uribe.canales@alumnos.sip.cl>
2022-04-12 11:06:44 -0400

    diagrama en T

---
+++ kimberly.js
@@ -0,0 +1,35 @@
+//Fragmento de código 1
+
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+   /* 
+    variables         Datos
+    auntcontactInfo     =    ["Paula","Smith","Calle principal 1234","St.louis", "MO",12345];
+   */
+
+//Fragmento de código 2
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+   /* 
+    variables  Datos  
+    produce    =    ["manzanas","naranjas"];
+    frozen     =    ["brocoli","helado"];
+   */
+
+//Fragmento de código 3
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+   /* 
+    variables    datos
+    movielibrary  =    ["bambi","E.T.", "Toy Story"];
+   */
