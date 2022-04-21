8e9f31bf367c5e956a0577cc033af0ccb0d49413
KiMberly006 <kimberly.uribe.canales@alumnos.sip.cl>
2022-04-21 09:36:20 -0400

    esta vez es

---
+++ esta vez es.js
@@ -0,0 +1,23 @@
+function muestraSiElNiñoPuedeSubirALaMontañaRusa (alturaNiño) {
+    if (alturaNiño >= 52) {
+      console.log ("¡Súbete, chico!")
+  }
+  else {
+  //  alturaniño < 52; {
+      console.log ("lo siento chico, intentalo el proximo año.")
+   // }
+  }
+  }
+  muestraSiElNiñoPuedeSubirALaMontañaRusa(50);
+  
+  function muestraSiElNiñoPuedeSubirALaMontañaRusa (alturaNiño) {
+    if (alturaNiño >= 55) {
+      console.log ("¡Súbete, chico!")
+  }
+  else {
+  //  alturaniño < 55; {
+      console.log ("¡Súbete, chico!")
+   // }
+  }
+  }
+  muestraSiElNiñoPuedeSubirALaMontañaRusa(55);
